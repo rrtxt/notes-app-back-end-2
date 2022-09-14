8143af245450d9929179bf60ba05685c107975bf
Rizfi <rizfiferdian@gmail.com>
2022-09-14 07:52:50 +0700

    data validation implement

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -2,9 +2,10 @@
 /* eslint-disable brace-style */
 /* eslint-disable no-underscore-dangle */
 class NotesHandler {
-  constructor(service) {
+  constructor(service, validator) {
     // eslint-disable-next-line no-underscore-dangle
     this._service = service;
+    this._validator = validator;
 
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -15,6 +16,7 @@ class NotesHandler {
 
   postNoteHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
  
       const noteId = this._service.addNote({ title, body, tags });
@@ -70,6 +72,7 @@ class NotesHandler {
 
   putNoteByIdHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.paylaod);
       const { id } = request.params;
       this._service.editNoteById(id, request.payload);
 
---
+++ src/validator/notes/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
